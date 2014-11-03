5362ed8ce80fa74a6adedcb0ba32ed58cfac155c
Sebastian Schrader <sebastian.schrader@tu-dresden.de>
2014-11-03 12:23:42 +0100

    Confirm per-item action support

--- static/custom/item-table.js
+++ static/custom/item-table.js
@@ -3,13 +3,26 @@ function responseHandler(response) {
 }
 
 var actionTemplate = _.template('<a class="btn btn-default" title="<%= title %>" href="<%= url %>"><span class="glyphicon <%= glyphicon %>"></span></a>');
+var confirmActionTemplate = _.template('<button class="btn btn-default confirm" type="button" value="<%= url %>"><span class="glyphicon <%= glyphicon %>"></span></button>');
 
-function actionFormatter(value, row, index) {
-    return _.map(value, function (url, action) {
-        return actionTemplate({
-            'title': itemActions[action].title,
-            'url': url,
-            'glyphicon': itemActions[action].glyphicon
+$('table').on('click', '.confirm', function() {
+    if (confirm("Sicher?")) {
+        window.location = $(this).attr('value');
+    }
+});
+
+function actionFormatter(urls, row, index) {
+    return _.map(perItemActions, function (action) {
+        var template;
+        if (action.confirm) {
+            template = confirmActionTemplate;
+        } else {
+            template = actionTemplate;
+        }
+        return template({
+            'title': action.title,
+            'url': urls[action.action],
+            'glyphicon': action.glyphicon
         });
     }).join('');
 }
