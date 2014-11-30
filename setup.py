437233fba1420d16f9cebf98df0f9a36ca518aad
Sebastian Schrader <sebastian.schrader@tu-dresden.de>
2014-11-30 22:20:05 +0100

    Create distribution information

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2
+from setuptools import setup
+
+setup(name='AdminLDAP',
+      version='0.1',
+      description='Manage AG DSN admins in a OpenLDAP server.',
+      author='Sebastian Schrader',
+      author_email='sebastian.schrader@wh2.tu-dresden.de',
+      packages=['adminldap'],
+      include_package_data=True,
+      zip_safe=False)
